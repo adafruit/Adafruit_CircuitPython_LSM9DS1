108130d0452729fc1482d372484a8e7437cfb4de
Kattni Rembor <kattni@adafruit.com>
2018-08-21 13:01:29 -0400

    Removed SPI from example.

--- examples/lsm9ds1_simpletest.py
+++ examples/lsm9ds1_simpletest.py
@@ -1,26 +1,13 @@
 # Simple demo of the LSM9DS1 accelerometer, magnetometer, gyroscope.
 # Will print the acceleration, magnetometer, and gyroscope values every second.
 import time
-
 import board
 import busio
-# import digitalio # Use when connected over SPI
-
 import adafruit_lsm9ds1
-
-
-# You have a couple options for wiring this sensor, either I2C (recommended)
-# or a SPI connection.  Choose _one_ option below and uncomment it:
 
 # I2C connection:
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
-
-# SPI connection:
-#spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-#xgcs = digitalio.DigitalInOut(board.D5)  # Pin connected to XGCS (accel/gyro chip select).
-#mcs  = digitalio.DigitalInOut(board.D6)  # Pin connected to MCS (magnetometer chip select).
-#sensor = adafruit_lsm9ds1.LSM9DS1_SPI(spi, xgcs, mcs)
 
 # Main loop will read the acceleration, magnetometer, gyroscope, Temperature
 # values every second and print them out.
