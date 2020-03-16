79e13b5021bd71d6a6fd6069a6d6117115e02623
dherrada <dylan.herrada@gmail.com>
2020-03-16 16:37:10 -0400

    Ran black, updated to pylint 2.x

--- examples/lsm9ds1_simpletest.py
+++ examples/lsm9ds1_simpletest.py
@@ -9,7 +9,7 @@ import adafruit_lsm9ds1
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
-#SPI connection:
+# SPI connection:
 # from digitalio import DigitalInOut, Direction
 # spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 # csag = DigitalInOut(board.D5)
@@ -29,9 +29,19 @@ while True:
     gyro_x, gyro_y, gyro_z = sensor.gyro
     temp = sensor.temperature
     # Print values.
-    print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
-    print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
-    print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
-    print('Temperature: {0:0.3f}C'.format(temp))
+    print(
+        "Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})".format(
+            accel_x, accel_y, accel_z
+        )
+    )
+    print(
+        "Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})".format(mag_x, mag_y, mag_z)
+    )
+    print(
+        "Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})".format(
+            gyro_x, gyro_y, gyro_z
+        )
+    )
+    print("Temperature: {0:0.3f}C".format(temp))
     # Delay for a second.
     time.sleep(1.0)
